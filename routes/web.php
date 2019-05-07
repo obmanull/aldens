e48737a57a8465fcb2c223f35a36c7a02074a0c0
z0mer <zer1word@gmail.com>
2019-05-07 06:58:57 +0300

    Add laravel email verification

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(
+    [
+        'middleware' => ['auth', 'verified']
+    ],
+    function () {
+        Route::get('/home', 'HomeController@index')->name('home');
+    }
+);
+
