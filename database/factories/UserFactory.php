b0568408cdb70b1b84d39bbe6cf9855dee88d63c
z0mer <zer1word@gmail.com>
2019-05-14 19:47:18 +0300

    Users seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,11 +16,12 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $active = $faker->boolean;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
+        'email_verified_at' => $active ? now() : null,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'remember_token' => $active ? Str::random(10) : null,
     ];
 });
